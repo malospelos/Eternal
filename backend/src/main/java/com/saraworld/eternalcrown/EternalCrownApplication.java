bbbfe288e35d671f11543ab433187cb4ff2d78d1
Malospelos <cjfernanp@gmail.com>
2026-08-09 18:48:15 +0200

    Añade aplicación Spring Boot

---
+++ backend/src/main/java/com/saraworld/eternalcrown/EternalCrownApplication.java
@@ -0,0 +1,12 @@
+package com.saraworld.eternalcrown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EternalCrownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EternalCrownApplication.class, args);
+    }
+}
