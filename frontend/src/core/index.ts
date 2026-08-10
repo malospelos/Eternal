9d1dc3c08d5e4e81ff55bf19a8acc8effdbf80bf
Malospelos <cjfernanp@gmail.com>
2026-08-10 13:46:00 +0200

    Exporta el núcleo de simulación Eternal

---
+++ frontend/src/core/index.ts
@@ -0,0 +1,6 @@
+export * from './PseudoRandom';
+export * from './WorldMap';
+export * from './Economy';
+export * from './ConstructionQueue';
+export * from './BattleSimulator';
+export * from './EternalSimulation';
